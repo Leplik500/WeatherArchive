451d3e56c5dff3431252c056bd6de86d4346b703
naotomo <nikitin-ao-dev@yandex.ru>
2025-03-13 16:34:00 +0300

    feature: pick range of months or years to filter

--- WeatherArchive/wwwroot/js/display.js
+++ WeatherArchive/wwwroot/js/display.js
@@ -1,18 +1,28 @@
-let selectedMonth = null;
-let selectedYear = null;
+let monthFrom = null;
+let monthTo = null;
+let yearFrom = null;
+let yearTo = null;
 
 $.fn.dataTable.ext.search.push(function (settings, data) {
     let dateString = data[0];
     let parts = dateString.split('.');
     if (parts.length !== 3) return false;
+    let day = parseInt(parts[0], 10);
     let month = parseInt(parts[1], 10) - 1;
     let year = parseInt(parts[2], 10);
+    let rowDate = new Date(year, month, day);
 
-    if (selectedMonth !== null && month !== selectedMonth) {
-        return false;
+    let monthOk = true;
+    if (monthFrom !== null && monthTo !== null) {
+        monthOk = rowDate.getMonth() >= monthFrom && rowDate.getMonth() <= monthTo;
     }
-    return !(selectedYear !== null && year !== selectedYear);
 
+    let yearOk = true;
+    if (yearFrom !== null && yearTo !== null) {
+        yearOk = rowDate.getFullYear() >= yearFrom && rowDate.getFullYear() <= yearTo;
+    }
+
+    return monthOk && yearOk;
 });
 
 let dataTable = $('#weatherTable').DataTable({
@@ -62,14 +72,17 @@ new AirDatepicker('#monthsPicker', {
     view: 'months',
     minView: 'months',
     dateFormat: 'MMMM',
+    range: true,
+    multipleDatesSeparator: ' - ',
     classes: 'month-only-picker',
-    onSelect({date}) {
-        if (date) {
-            selectedMonth = date.getMonth();
+    onSelect({date: dates}) {
+        if (dates && dates.length > 0) {
+            monthFrom = dates[0].getMonth();
+            monthTo = dates.length > 1 ? dates[1].getMonth() : monthFrom;
         } else {
-            selectedMonth = null;
+            monthFrom = null;
+            monthTo = null;
         }
-
         dataTable.draw();
     }
 });
@@ -78,14 +91,17 @@ new AirDatepicker('#yearsPicker', {
     view: 'years',
     minView: 'years',
     dateFormat: 'yyyy',
+    range: true,
+    multipleDatesSeparator: ' - ',
     startDate: '01.01.2010',
-    onSelect({date}) {
-        if (date) {
-            selectedYear = date.getFullYear();
+    onSelect({date: dates}) {
+        if (dates && dates.length > 0) {
+            yearFrom = dates[0].getFullYear();
+            yearTo = dates.length > 1 ? dates[1].getFullYear() : yearFrom;
         } else {
-            selectedYear = null;
+            yearFrom = null;
+            yearTo = null;
         }
-
         dataTable.draw();
-    }
+    },
 });
